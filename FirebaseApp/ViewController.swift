0946e8e0f80b6c0d561deaa4a202a8c366e972ba
Elisei Mishchenko <esm.mail@icloud.com>
2022-02-11 17:30:02 +0300

    Initial Commit

---
+++ FirebaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by Елисей Мищенко on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
