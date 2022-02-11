b6309ba473dbc455439e6a4b298e43177976bc99
Elisei Mishchenko <esm.mail@icloud.com>
2022-02-11 18:13:08 +0300

    Creating a SignInViewController file and renaming the LogInViewController file adding IBOutlets for textFields

---
+++ FirebaseApp/LogInViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by Елисей Мищенко on 11.02.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func unwind(for seque: UIStoryboardSegue) {
+        
+    }
+
+}
+
---
+++ FirebaseApp/SignInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignInViewController.swift
+//  FirebaseApp
+//
+//  Created by Елисей Мищенко on 11.02.2022.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
