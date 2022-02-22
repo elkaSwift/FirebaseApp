d8a2b9b46041624cc9e95baf8a3605984748c55e
Elisei Mishchenko <esm.mail@icloud.com>
2022-02-22 18:48:01 +0300

    I have written the logic for user registration

--- FirebaseApp/SignInViewController.swift
+++ FirebaseApp/SignInViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInViewController: UIViewController {
     
@@ -15,6 +16,26 @@ class SignInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Error", message: "Fill in all the fields", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func signInButton(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = passwordTextField.text, email != "", password != "" else {
+            showAlert()
+            return
+        }
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] user, error in
+            if error == nil {
+                if user != nil {
+                    self?.performSegue(withIdentifier: "signin", sender: nil)
+                }
+            }
+        }
     }
     
 }
